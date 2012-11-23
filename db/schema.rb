a80398bc5a40f3289397782f4f4f259b747acd96
Till Krueger <dragon2@hotmail.de>
2012-11-23 14:58:01 +0100

    added Birthday

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121123125455) do
+ActiveRecord::Schema.define(:version => 20121123133936) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(:version => 20121123125455) do
     t.string   "username"
     t.string   "firstname"
     t.string   "lastname"
+    t.date     "birthdate"
+    t.date     "birthday"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
