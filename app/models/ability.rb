d65a004d131113580a7b1996d420809e625dbdfb
Tobias <tobias.prein@googlemail.com>
2013-01-11 12:55:25 +0100

    right mgmt added. bugs fixed. lesser functions in tiny mce.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,11 +2,13 @@ class Ability
   include CanCan::Ability
   def initialize(user)
     user ||= User.new # guest user
-        if user.role? :admin
-          can :manage, :all
-        end
-        if user.role? :user
-          can :manage, Message
+      if user.role? :admin
+        can :manage, :all
+      else
+        can :read, :all
+      end
+      if user.role? :user
+        can :manage, Message
       end
     end
 end
