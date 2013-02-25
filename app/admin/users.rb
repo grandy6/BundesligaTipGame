06d0d29e32cd4232db71f615d8507925e380e10e
Tobias <tobias.prein@googlemail.com>
2013-02-25 16:43:10 +0100

    adding teams and users to activeadmin. fixed seed problem with adminuser.

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register User do
-  
+  index do
+    column :id
+    column :username
+    column :firstname
+    column :lastname
+    column :email
+    column :created_at
+    column :updated_at
+    default_actions
+  end
 end
