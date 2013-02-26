feb861cbda3ec0d53f1f0196080b414fd008c264
Tobias <tobias.prein@googlemail.com>
2013-02-26 16:23:00 +0100

    solved problems with activeadmin.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :firstname, :lastname, :birthday, :roles, :role_ids
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :firstname, :lastname, :birthday, :roles
   
   # validates_uniqueness_of :username
 
