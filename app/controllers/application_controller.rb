fa9b272b55adf1a2fa4949d75e07ae0eb083372c
Tobias <tobias.prein@googlemail.com>
2013-02-27 16:02:26 +0100

    hopefully fixed too many redirect error in application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
     if user_signed_in?
       redirect_to start_index_path, :alert => exception.message 
   	else
-  		redirect_to root_url, :alert => exception.message
+  		redirect_to start_index_path, :alert => exception.message
   	end
 	end
 end
