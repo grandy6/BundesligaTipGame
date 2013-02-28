c7c51bf8b978ea416923fc1d1cbd68c5b3a30e5a
Tobias <tobias.prein@googlemail.com>
2013-02-28 15:24:52 +0100

    fixed bug in model news.rb.

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,5 +1,5 @@
 class News < ActiveRecord::Base
-  attr_accessible :content, :title, :user_id
+  attr_accessible :content, :title, :user_id, :category_ids
   has_many :news_categories
   has_many :categories, through: :news_categories
   belongs_to :user
--- app/models/news_category.rb
+++ app/models/news_category.rb
@@ -1,4 +1,5 @@
 class NewsCategory < ActiveRecord::Base
+  attr_accessible :news, :category
   belongs_to :news
   belongs_to :category
 end
